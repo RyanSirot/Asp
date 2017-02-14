45beace5c2599b5d60ad32cd9da37cfc8fdde5d5
RyanSirot <torisnayr@gmail.com>
2017-02-13 23:14:46 -0800

    comment

--- ZenithWebSite/Controllers/HomeController.cs
+++ ZenithWebSite/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace ZenithWebSite.Controllers
             startOfWeek = startOfWeek.AddDays(1);
             DateTime endOfWeek = startOfWeek.AddDays(7);
 
-            var events = db.Events//.Where(e => e.FromDateTime >= startOfWeek && e.ToDateTime <= endOfWeek && e.IsActive == true)
+            var events = db.Events.Where(e => e.FromDateTime >= startOfWeek && e.ToDateTime <= endOfWeek && e.IsActive == true)
                                 .OrderBy(d => d.FromDateTime)
                                 .Select(e => e);
             ViewBag.activities = db.Activities.Select(e => e);
